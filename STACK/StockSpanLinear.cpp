2db4b408fa103c485b6e45def1f4e96762118e39
SUVIGHYA DUBEY <suvighyad@gmail.com>
2021-09-02 11:42:42 +0530

    STOCK_SPAN_LINEAR_COMPPLEXITY

--- STACK/StockSpanLinear.cpp
+++ STACK/StockSpanLinear.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 void calculateSpan(int price[], int n, int S[])
 {
+    stack<int> st;
+    st.push(0);
     S[0] = 1;
     for (int i = 1; i < n; i++)
     {
-        S[i] = 1;
-        for (int j = i - 1; (j >= 0) && (price[i] >= price[j]); j--)
-        {
-            S[i]++;
-        }
+        while (!st.empty() && price[st.top()] <= price[i])
+            st.pop();
+        S[i] = (st.empty()) ? (i + 1) : (i - st.top());
+
+        st.push(i);
     }
 }
 void printArray(int arr[], int n)
